a2f9f98623ccc13adfd95440b80d9fdae5e2b0b5
Julian Weidert <JulianWeidert@users.noreply.github.com>
2022-10-23 00:07:17 +0200

    Created window in main

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+#include <memory>
+
+#include <PixelWindow/PixelWindow.h>
 
 int main() {
 
-	std::cout << "Hello World" << std::endl;
+	std::shared_ptr<pw::PixelWindow> window = std::make_shared<pw::PixelWindow>(640, 480, "Ray Tracer", false);
 
+	window->makeCurrent();
+	while (window->isActive()) {
+
+		
+		window->swapBuffers();
+
+		window->pollEvents();
+	}
+	
 	return 0;
 }
